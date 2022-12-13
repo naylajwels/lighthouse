de0069f2348c83387e61fc47f818651d118caa7d
Connor Clark <cjamcl@google.com>
2022-12-13 13:50:10 -0800

    new_audit: inputs-can-be-pasted-into

--- cli/test/smokehouse/test-definitions/dobetterweb.js
+++ cli/test/smokehouse/test-definitions/dobetterweb.js
@@ -341,7 +341,7 @@ const expectations = {
           ],
         },
       },
-      'password-inputs-can-be-pasted-into': {
+      'inputs-can-be-pasted-into': {
         score: 0,
         details: {
           items: {
