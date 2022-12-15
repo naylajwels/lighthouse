8ec7e0bc24264d7c1d534f666803932236f22e7a
Alex N. Jose <alexnj@google.com>
2022-12-15 07:23:42 -0800

    Update branch to reflect changes in entity-based-3p branch

--- report/renderer/report-renderer.js
+++ report/renderer/report-renderer.js
@@ -270,6 +270,7 @@ export class ReportRenderer {
 
     const detailsRenderer = new DetailsRenderer(this._dom, {
       fullPageScreenshot: report.fullPageScreenshot ?? undefined,
+      entityClassification: report.entityClassification,
     });
 
     const categoryRenderer = new CategoryRenderer(this._dom, detailsRenderer);
